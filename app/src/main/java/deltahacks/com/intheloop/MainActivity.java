7137a156a3925e13131e1027d34af6ba81322bd5
oliviakim95 <oliviakim321@gmail.com>
2015-02-15 04:10:16 -0500

    Adding history fragment template

--- app/src/main/java/deltahacks/com/intheloop/MainActivity.java
+++ app/src/main/java/deltahacks/com/intheloop/MainActivity.java
@@ -174,7 +174,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_history, container, false);
             return rootView;
         }
 
